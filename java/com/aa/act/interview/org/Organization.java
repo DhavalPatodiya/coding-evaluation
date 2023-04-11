3a40915ad587d06a9260105999aff6d37e564e9d
DhavalPatodiya <patodiya25@gmail.com>
2023-04-11 16:08:21 -0700

    Completing assessment

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,10 +1,14 @@
 package com.aa.act.interview.org;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayDeque;
 import java.util.Optional;
+import java.util.Queue;
 
 public abstract class Organization {
 
 	private Position root;
+	private int identifier = 0;
 	
 	public Organization() {
 		root = createOrganization();
@@ -21,6 +25,26 @@ public abstract class Organization {
 	 */
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
+		// if position is CEO
+		if(root.getTitle().equals(title)){
+			// if position is not filled then fill the position
+			return hire(person, root);
+		}
+
+		// iterating through all positions via bfs graph
+		Queue<Position> q = new ArrayDeque<>();
+		q.offer(root);
+		while(!q.isEmpty()){
+			Position boss = q.poll();
+			for(Position pos : boss.getDirectReports()){
+				if(pos.getTitle().equals(title)){
+					return hire(person, pos);
+				}
+				q.offer(pos);
+			}
+		}
+
+		identifier -= 1;
 		return Optional.empty();
 	}
 
@@ -28,7 +52,8 @@ public abstract class Organization {
 	public String toString() {
 		return printOrganization(root, "");
 	}
-	
+
+	//region PRIVATE METHODS
 	private String printOrganization(Position pos, String prefix) {
 		StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
 		for(Position p : pos.getDirectReports()) {
@@ -36,4 +61,19 @@ public abstract class Organization {
 		}
 		return sb.toString();
 	}
+
+	private Optional<Position> hire(Name person, Position pos){
+		// generating unique identifier for each employee getting hired
+		identifier += 1;
+		Employee emp = new Employee(identifier, person);
+		if(!pos.isFilled()) {
+			pos.setEmployee(Optional.of(emp));
+			return Optional.of(pos);
+		}
+
+		identifier -= 1;
+		return Optional.empty();
+	}
+
+	//endregion
 }
